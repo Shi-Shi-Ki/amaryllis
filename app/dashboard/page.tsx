9b9b1b3c877b09ffaa56563c6ef1e0f6f5837fb3
獅四季 <32070896+Shi-Shi-Ki@users.noreply.github.com>
2025-04-29 16:33:30 +0900

    dashboardのレイアウト修正 (#8)

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -45,7 +45,7 @@ export default function Dashboard() {
       </div>
       <div className="drawer-side">
         <label htmlFor="my-drawer" aria-label="close sidebar" className="drawer-overlay"></label>
-        <ul className="menu bg-base-200 text-base-content min-h-full w-80 p-4">
+        <ul className="menu bg-base-200 text-base-content min-h-full w-[50rem] p-4">
           {/* Sidebar content here */}
           {selectedItem && (
             <>
